2159b2b1e65979950b1293241eb892d7202cc4a5
jerson.seling <jerson.seling@softplan.com.br>
2021-07-10 21:19:27 -0300

    texture finalization

---
+++ Delphi/GLFW/004e2-Learn-Textures - Wrapping/L04z2_Wrapping.dpr
@@ -0,0 +1,183 @@
+program L04z2_Wrapping;
+
+uses
+  System.StartUpCopy,
+  System.SysUtils,
+  Winapi.OpenGL,
+  Winapi.OpenGLExt,
+  System.Math.Vectors,
+  Neslib.Glfw3 in '..\Glfw\Neslib.Glfw3.pas',
+  uShader in 'uShader.pas',
+  uTextures in 'uTextures.pas';
+
+//  uImageHandler in 'uImageHandler.pas';
+
+const
+  VERTICES: array [0..31] of single = (
+    // positions          // colors           // texture coords
+     0.5,  0.5, 0.0,   1.0, 0.0, 0.0,   2.0, 2.0,   // top right
+     0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   2.0, 0.0,   // bottom right
+    -0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0, 0.0,   // bottom left
+    -0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0, 2.0    // top left
+  );
+
+const
+ INDICES: array [0..5] of integer = (  // note that we start from 0!
+    0, 1, 3,   // first triangle
+    1, 2, 3    // second triangle
+  );
+
+procedure ErrorCallback(error: Integer; const description: PAnsiChar); cdecl;
+var
+  Desc: String;
+begin
+  Desc := String(AnsiString(description));
+  raise Exception.CreateFmt('GLFW Error %d: %s', [error, Desc]);
+end;
+
+procedure KeyCallback(window: PGLFWwindow; key, scancode, action, mods: Integer); cdecl;
+begin
+  if (key = GLFW_KEY_ESCAPE) and (action = GLFW_PRESS) then
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+end;
+
+procedure Run;
+var
+  Window: PGLFWwindow;
+  VBO: GLuint;
+  VAO: GLuint;
+  EBO: GLuint;
+  texture1, texture2: GLuint;
+  AShader: IShader;
+  Width, Height: Integer;
+  info: TImageInfo;
+begin
+  glfwSetErrorCallback(ErrorCallback);
+
+  //inicializa a GLFW
+  if (glfwInit = 0) then
+    raise Exception.Create('Unable to initialize GLFW');
+
+  //configura a GLFW
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  //cria a janela
+  Window := glfwCreateWindow(800, 600, 'LearnOpenGL', nil, nil);
+  if (Window = nil) then
+  begin
+    glfwTerminate;
+    raise Exception.Create('Failed to create GLFW window');
+  end;
+
+//  glfwSetKeyCallback(Window, KeyCallback);
+
+  //define a janela como o contexto atual
+  glfwMakeContextCurrent(Window);
+//  glfwSwapInterval(1);
+
+  // NOTE: OpenGL error checks have been omitted for brevity
+
+  InitOpenGLext;
+
+  glGenVertexArrays(1, @VAO);
+  glBindVertexArray(VAO);
+
+  //Gera um buffer e vincula ele a uma ID armazenada dentro da variavel VBO.
+  //O 1 aqui é qtd de buffers a ser gerada.
+  glGenBuffers(1, @VBO);
+  //o tipo do VBO é GL_ARRAY_BUFFER. Aqui define-se que o buffer é um VBO mesmo
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  //carrega os vértices no VBO
+  glBufferData(GL_ARRAY_BUFFER, SizeOf(VERTICES), @VERTICES, GL_STATIC_DRAW);
+
+  glGenBuffers(1, @EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, SizeOf(INDICES), @INDICES, GL_STATIC_DRAW);
+
+
+  /////////////////////////
+  AShader := TShader.Create('3.3.shader.vs', '3.3.shader.fs');
+
+//texture 1
+  glGenTextures(1, @texture1);
+  glBindTexture(GL_TEXTURE_2D, texture1);
+
+  // set the texture wrapping parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // note that we set the container wrapping method to GL_CLAMP_TO_EDGE
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  // set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load and generate the texture
+
+  info := LoadTexture('container.bmp');
+  glTexImage2d(GL_TEXTURE_2D, 0, GL_RGB, info.width, info.height, 0, GL_RGB, GL_UNSIGNED_BYTE, info.pdata);
+  glGenerateMipmap(GL_TEXTURE_2D);
+  FreeMem(info.pdata);
+
+//texture 2
+  glGenTextures(1, @texture2);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+  
+  // set the texture wrapping parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// we want to repeat the awesomeface pattern so we kept it at GL_REPEAT
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // load and generate the texture
+
+  info := LoadTexture('awesomeface.tga');
+  glTexImage2d(GL_TEXTURE_2D, 0, GL_RGBA, info.width, info.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, info.pdata);
+  glGenerateMipmap(GL_TEXTURE_2D);
+  FreeMem(info.pdata);
+
+  AShader.Use();
+  AShader.SetUniform1i('texture2', 1);
+
+  //vincula um vertice a variavel 0 do shader
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(Single), pointer(0));
+  glEnableVertexAttribArray(0);
+
+  // color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(Single), pointer(3 * sizeof(Single)));
+  glEnableVertexAttribArray(1);
+
+  //texture coordinate
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(Single), pointer(6 * sizeof(Single)));
+  glEnableVertexAttribArray(2);
+
+  while (glfwWindowShouldClose(Window) = 0) do
+  begin
+    glfwGetFramebufferSize(Window, @Width, @Height);
+
+    glViewport(0, 0, Width, Height);
+
+    glClearColor(0.2, 0.3, 0.3, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    
+    AShader.Use();
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nil);
+
+    glfwSwapBuffers(Window);
+    glfwPollEvents;
+  end;
+
+  glfwDestroyWindow(Window);
+  glfwTerminate;
+end;
+
+begin
+  Run;
+end.
